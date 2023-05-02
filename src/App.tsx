a451cead9214a132ab9693627338e059c33f9221
Michael Urich <urich.michael@outlook.com>
2023-05-02 23:29:50 +0200

    added data fetching with useEffect the wrong way

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+type AsyncState = 'ideal' | 'loading' | 'succeeded' | "failed"
+type Data = { "id": number; "name": string; "altName": string }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isFetching, setIsFetching] = useState<AsyncState>('ideal')
+  const [data, setData] = useState<Data[] | undefined>()
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsFetching('loading')
+      try {
+        const data = await axios.get('http://localhost:4000/superheros')
+        if (data) {
+          setIsFetching('succeeded')
+          setData(data.data)
+        }
+      } catch (e) {
+        setIsFetching('failed')
+      }
+    }
+    const cancelCall = getData()
+    if (isFetching === 'failed') {
+      return () => { cancelCall }
+    }
+  }, [])
 
   return (
 
-    <>
-      <div className='flex justify-center items-center'>
-        hello {count}
+    <div className='w-screen bg-red-500'>
+      <div >
+        {JSON.stringify(data)}
       </div>
 
-      <button onClick={() => setCount(prev => prev + 1)}>Count</button>
-    </>
+
+    </div>
   )
 }
 
